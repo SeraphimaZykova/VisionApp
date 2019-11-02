55912aca46922a7ae74c3154ac353e88ddf5d758
serafima <zykovas91@gmail.com>
2019-11-02 11:37:15 +0300

    project created

---
+++ VisionApp/VisionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisionApp
+//
+//  Created by Серафима Зыкова on 02/11/2019.
+//  Copyright © 2019 Серафима Зыкова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
